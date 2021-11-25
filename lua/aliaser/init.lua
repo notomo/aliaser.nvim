225bbe7d3efaed1f7bf2bf6f066046a4d9ced5fa
notomo <notomo.motono@gmail.com>
2021-11-25 10:25:07 +0900

    Improve doc

--- lua/aliaser/init.lua
+++ lua/aliaser/init.lua
@@ -10,6 +10,7 @@ function M.register_factory(ns, fn)
 end
 
 --- Gets registered aliases.
+--- @return table: {name = (string), file_path = (string), start_row = (number)}[]
 function M.list()
   return Command.new("list")
 end
@@ -17,12 +18,14 @@ end
 --- Call an alias by name.
 --- @param name string: alias name
 --- @vararg any: alias function arguments
+--- @return any: alias function result
 function M.call(name, ...)
   return Command.new("call", name, ...)
 end
 
 --- Converts an alias to string.
 --- @param alias table: alias object
+--- @return string: lua expression to call the alias
 function M.to_string(alias)
   return Command.new("to_string", alias)
 end
