3e0d728197d3e7267840b8bdea6dda95f0782563
notomo <notomo.motono@gmail.com>
2022-09-26 12:43:53 +0000

    Update vendor (assert.buffer_name)

--- lua/aliaser/vendor/assertlib/function/buffer.lua
+++ lua/aliaser/vendor/assertlib/function/buffer.lua
@@ -8,4 +8,32 @@ function M.filetype()
   return vim.bo.filetype
 end
 
+function M.buffer_full_name()
+  return vim.api.nvim_buf_get_name(0)
+end
+
+function M.buffer_name_tail()
+  local full_name = vim.api.nvim_buf_get_name(0)
+  return vim.fn.fnamemodify(full_name, ":t")
+end
+
+function M.buffer_number()
+  return vim.api.nvim_get_current_buf()
+end
+
+M.exists_pattern = {
+  get_expected = function(pattern)
+    return pattern:gsub("\n", "\\n")
+  end,
+  is_ok = function(result)
+    return vim.fn.search(result.expected, "n") ~= 0
+  end,
+  positive_message = function(result)
+    return ("`%s` not found"):format(result.expected)
+  end,
+  negative_message = function(result)
+    return ("`%s` found"):format(result.expected)
+  end,
+}
+
 return M
